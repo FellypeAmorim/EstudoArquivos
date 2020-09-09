5852e8c0dad05fcce80073c3cfa0d843d85875a4
Fellype Amorim <fellype.amorim88@gmail.com>
2020-09-09 15:43:43 -0300

    Inicio de estudo sobre arquivos, criado sitema de copia de arquivos e criado um vetor para guardar dados de um arquivo

---
+++ EstudoArquivos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace EstudoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string soucePath = @"C:\Users\gigim\Desktop\C#\Temp\file1.txt";
+            string targetPath = @"C:\Users\gigim\Desktop\C#\Temp\file2.txt";
+            
+            try
+            {
+                FileInfo fileinfo = new FileInfo(soucePath);
+                fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(soucePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
